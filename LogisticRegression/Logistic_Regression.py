0c6192f12fc39c4ffb55bea5fe84ef82a2924227
prathamesh201999 <63088560+prathamesh201999@users.noreply.github.com>
2020-04-30 15:18:42 +0530

    Create Logistic_Regression.py

---
+++ LogisticRegression/Logistic_Regression.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Social_Network_Ads.csv')
+
+x = df.iloc[:,[2,3]].values
+y = df.iloc[:,4].values
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.25, random_state = 0)
+
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
+from sklearn.linear_model import LogisticRegression
+reg = LogisticRegression()
+reg.fit(x_train,y_train)
+y_pred = reg.predict(x_test)
+
+
+from sklearn.metrics import confusion_matrix
+con = confusion_matrix(y_test,y_pred)
+
+
+from matplotlib.colors import ListedColormap
+x_set,y_set = x_train,y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:,0].min() -1, stop = x_set[:,0].max() +1,step = 0.01),
+                     np.arange(start = x_set[:,1].min() -1,stop = x_set[:,1].max() +1,step = 0.01))
+plt.contourf(x1,x2, reg.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(x1.min(), x1.max())
+plt.xlim(x2.min(), x2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j,0],x_set[y_set ==j, 1],
+                c = ListedColormap(('red','green'))(i),label = j)
+plt.legend()
+plt.show()
+
+
+
